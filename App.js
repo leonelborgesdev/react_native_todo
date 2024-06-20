2bd3aed95fa857dfec5ceddec5cae5d3ef49fc5b
leonelborgesdev <leonel03t@gmail.com>
2024-06-20 10:59:13 -0400

    styles text

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hola Leo!</Text>
+      <TextInput style={styles.text} />
     </View>
   );
 }
@@ -13,8 +13,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    borderColor: "#999",
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 120,
+    height: 40,
   },
 });
